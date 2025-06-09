32b2e3dfcf5addccb12972880a2246dbe4b1d577
PeterSteve <pedropote677@gmail.com>
2025-06-09 01:24:46 -0300

    Update id000-000-000.c

--- BackupPersists.c/id000-000-000.c
+++ BackupPersists.c/id000-000-000.c
@@ -121,24 +121,3 @@ void load_generator_state(FileIDGenerator *gen, const char *path) {
         gen->counter = 0;
     }
 }
-
-
-
-int main(void)
-{
-    FileIDGenerator gen;
-
-    // Carrega o estado do gerador (opcional)
-    load_generator_state(&gen, "estado.bin");
-
-    // Escolha o diretório inicial (exemplo: "C:/Users/rdafa")
-    const char *diretorio_inicial = "C:/Users/rdafa";
-
-    // Vasculha as pastas e gera IDs
-    scan_and_generate(diretorio_inicial, &gen);
-
-    // Salva o estado do gerador ao final (opcional)
-    save_generator_state(&gen, "estado.bin");
-
-    return 0;
-}
