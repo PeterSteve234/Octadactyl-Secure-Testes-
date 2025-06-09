8ee8865f6e6b093d9190e8616afdad72558cb46e
PeterSteve <pedropote677@gmail.com>
2025-06-09 01:47:36 -0300

    Update Backup.c

--- BackupPersists.c/Backup.c
+++ BackupPersists.c/Backup.c
@@ -1,38 +1,32 @@
-
-
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <direct.h> // Para _mkdir no Windows
+#include <windows.h> // Para verificar atributos de arquivos
 // Caminho padrão para backup
 #define CAMINHO_PADRAO "C:\\MeusBackups
+#define TAM_MAX_CAMINHO 260
 
-int main() {
-    char resposta[10];
-    char caminhoBackup[260];
-
-        printf(" Deseja fazer Backup agora? (S/n)");
-        fgets(resposta, sizeof(resposta), stdin);
-
-       // remove o \n do final
-       reposta[strcspn(resposta, "\n")] = 0;
-
-       if  (strcspn(resposta, "s") == 0 || strcmp(resposta, "S") == 0) {
-
-            printf("Digite o caminho onde quer salvar o Backup (pressione enter para usar '%s'):\n" , CAMINHO_PADRAO);
-            fgets(caminhoBackup, sizeof(CaminhoBackup), stdin);
-            caminhoBackup[strscpy(CaminhoBackup, "\n")] = 0;
-
-        if (strlen(caminhoBackup) == 0) {
-          strcpy(caminhoBackup, CAMINHOPADRAO)
+void criarDiretorioSeNaoExistir(const char *caminho) {
+    if (_access(caminho, 0)) { // Verifica se o diretório não existe
+        if (_mkdir(caminho)) { // Tenta criar o diretório
+            perror("Erro ao criar diretorio %s\n", caminho);
+            exit(1);
         }
-
-         printf("Backup sera salvo em: %s\n", CaminhoBackup);
-         // codigo futuro será jogado aqui 
-    } else {
-         printf("Backup cancelado pelo usuario. \n");
     }
-           
-  return 0;
 }
+int main () {
+  char resposta[10];
+ char caminhoBackup[TAM_MAX_CAMINHO];
+  char caminhoOrigem[TAM_MAX_CAMINHO];
+  printf("Deseja fazer o Backup dos arquivos? (s/n):");
+  // Lê a resposta do usuário
+     fgets(resposta, sizeof(resposta), stdin);
+           resposta[strcspn(resposta, "\n")] = 0;
+      if (strcmp(resposta, "s") == 0 || strcmp(resposta, "S") == 0) {
+        printf("Digite o caminho de origem onde quer
+        salvar o backup (PRESSIONE ENTER para usar '%s'):\n " , CAMINHO_PADRAO);
+        fgets(caminhoOrigem, sizeof(caminhoOrigem), stdin);
+        caminhoBackup[strcspn(caminhoOrgiem, "\n")] = '\0';
+        } 
+      }
