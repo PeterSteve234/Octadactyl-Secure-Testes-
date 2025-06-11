c5c8e04e96ddc458cd0a392da6cc80d519277b63
PeterSteve <pedropote677@gmail.com>
2025-06-11 01:21:15 -0300

    Update linuxKernel.c

--- drivers/linux/linuxKernel.c
+++ drivers/linux/linuxKernel.c
@@ -1,11 +1,75 @@
-#include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/proc_fs.h>
-#include <linux/uaccess.h>
+#include <linux/init.h>
+#include <linux/lsm_hooks.h>
+#include <linux/netlink.h>
+#include <net/sock.h>
+#include <linux/skbuff.h>
+#include <linux/sched.h>
+#include <linux/string.h>
 
 
-#define PROC_NAME "Security_driver_acess"
+#define NETLINK_USER 31
 
-static char message[32] "Driver iniciado com segurança!\n";
-// restante do codigo aqui
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Peter Steve");
+MODULE_DESCRIPTION("LSM + Netlink Antivirus Control");
+
+static struct sock *nl_sk = NULL;
+
+struct trusted_process {
+    const char*name;
+};
+
+static struct trusted_process trusted_list[] ={
+  {"systemd"},
+    {"bash"},
+    {"sshd"},
+    {"init"},
+    {"kthreadd"},
+    {"ksoftirqd"},
+    {"rcu_sched"},
+    {"rcu_bh"},
+    {"migration/0"},
+    {"watchdog/0"},
+    {"kworker/0:0"},
+};
+
+#define TRUSTED_LIST_SIZE (sizeof(trusted_list)/sizeof(trusted_list[0]))
+
+static int is_process_trusted(const char *proc_name) {
+    int i;
+    for (i = 0; i < TRUSTED_LIST_SIZE; i++) {
+        if (strcmp(proc_name, trusted_list[i].name) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+static void send_netlink_message(const char *msg, u32 pid) {
+    struct sk_buff *skb_out;
+    struct nlmsghdr *nlh;
+    int msg_size = strlen(msg);
+    int res;
+
+    skb_out = nlmsg_new(msg_size, GFP_KERNEL);
+    if(!skb_out) {
+        pr_err("Failed to allocate new skb\n");
+        return;
+    }
+
+    nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);
+    NETLINK_CB(skb_out).dst_group = 0;
+    memcpy(nlmsg_data(nlh), msg, msg_size);
+
+
+    res = nlmsg_unicast(nl_sk, skb_out, pid);
+    if(res < 0) {
+        pr_err("error while sending back to user\n");
+        pr_err("nlmsg_unicast error: %d\n", res);
+        kfree_skb(skb_out);
+        
+    }
+}
